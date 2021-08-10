efd45e368f8902c2ab1ca140858cdc5b18dbdd18
LRojon <loic.rojon@gmail.com>
2021-08-10 18:58:55 +0200

    Add Daily request

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const characterRoutes = require('./routes/characterController')
 const elementRoutes = require('./routes/elementController')
 const weaponRoutes = require('./routes/weaponController')
 const materialRoutes = require('./routes/materialController')
-const dailyRoutes = require('./routes/dailyController')
 
 app.use('/characters', characterRoutes)
 app.use('/elements', elementRoutes)
 app.use('/weapons', weaponRoutes)
 app.use('/materials', materialRoutes)
-app.use('/daily', dailyRoutes)
 
 app.listen(5500, () => console.log("Server started at port 5500"));
--- routes/materialController.js
+++ routes/materialController.js
@@ -24,6 +24,7 @@ router.get('/:lang/all', (req, res) => {
                 {
                     let materials = genshin.materials('names', {matchCategories: true, verboseCategories: true, resultLanguage: 'English'})
                     materials.forEach(material => {
+                        material['inserDate'] = Date.now()
                         MaterialENModel.findOneAndReplace({name: material.name}, material).exec()
                     })
                 }
@@ -55,6 +56,7 @@ router.get('/:lang/all', (req, res) => {
                 {
                     let materials = genshin.materials('names', {matchCategories: true, verboseCategories: true, resultLanguage: 'French'})
                     materials.forEach(material => {
+                        material['inserDate'] = Date.now()
                         MaterialFRModel.findOneAndReplace({name: material.name}, material).exec()
                     })
                 }
@@ -66,6 +68,82 @@ router.get('/:lang/all', (req, res) => {
                 res.send(docs)
             })
         }, 100);
+    }
+})
+
+router.get('/:lang/daily', (req, res) => {
+    if(req.params.lang == 'en-us')
+    {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        let day = daysOfWeek[(new Date()).getDay()];
+        MaterialENModel.findOne({daysofweek: day}, (err, docs) => {
+            if(err) console.log("Daily material find error : " + err)
+            if(!docs)
+            {
+                let materials = genshin.materials(day, {matchCategories: true, verboseCategories: true, resultLanguage: 'English'})
+                materials.forEach(material => {
+                    let tmp = new MaterialENModel(material)
+                    tmp.save((err, docs) => {
+                        if(err) console.log("Material save error : " + err)
+                    })
+                })
+            }
+            else
+            {
+
+                if((new Date()).getTime() - docs.insertDate.getTime() > 10000/*86400000*/)
+                {
+                    let materials = genshin.materials(day, {matchCategories: true, verboseCategories: true, resultLanguage: 'English'})
+                    materials.forEach(material => {
+                        material['insertDate'] = Date.now()
+                        let tmp = new MaterialENModel(material)
+                        tmp.save((err, docs) => {
+                            if(err) console.log("Material save error : " + err)
+                        })
+                    })
+                }
+            }
+        })
+        MaterialENModel.find({rarity: '2', daysofweek: day}, (err, docs) => {
+            if(err) console.log("Daily material find error : " + err)
+            else res.send(docs)
+        })
+    }
+    else if(req.params.lang == 'fr-fr')
+    {
+        const daysOfWeek = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Venredi', 'Samedi']
+        let day = daysOfWeek[(new Date()).getDay()];
+        MaterialFRModel.findOne({daysofweek: day}, (err, docs) => {
+            if(err) console.log("Daily material find error : " + err)
+            if(!docs)
+            {
+                let materials = genshin.materials(day, {matchCategories: true, verboseCategories: true, resultLanguage: 'French'})
+                materials.forEach(material => {
+                    let tmp = new MaterialFRModel(material)
+                    tmp.save((err, docs) => {
+                        if(err) console.log("Material save error : " + err)
+                    })
+                })
+            }
+            else
+            {
+                if((new Date()).getTime() - docs.insertDate.getTime() > 10000/*86400000*/)
+                {
+                    let materials = genshin.materials(day, {matchCategories: true, verboseCategories: true, resultLanguage: 'French', queryLanguages: ['French']})
+                    materials.forEach(material => {
+                        material['insertDate'] = Date.now()
+                        let tmp = new MaterialFRModel(material)
+                        tmp.save((err, docs) => {
+                            if(err) console.log("Material save error : " + err)
+                        })
+                    })
+                }
+            }
+        })
+        MaterialFRModel.find({rarity: '2', daysofweek: day}, (err, docs) => {
+            if(err) console.log("Daily material find error : " + err)
+            else res.send(docs)
+        })
     }
 })
 
