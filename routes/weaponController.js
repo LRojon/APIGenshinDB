0ef3d5d3b552676ed47b1184ad02bce6695b653e
LRojon <loic.rojon@gmail.com>
2021-08-10 13:35:04 +0200

    Add Weapon (fr) request

--- routes/weaponController.js
+++ routes/weaponController.js
@@ -80,7 +80,76 @@ router.get('/:lang/all', (req, res) => {
     }
     else if (req.params.lang == 'fr-fr')
     {
-
+        WeaponFRModel.findOne({}, (err, docs) => {
+            if(err) console.log('Weapon find error : ' + err)
+            if(!docs) {
+                let weapons = genshin.weapons('names', {matchCategories: true, verboseCategories: true, resultLanguage: 'French'})
+                weapons.forEach(weapon => {
+                    let stats = []
+                    let lvlMax = weapon.rarity > 2 ? 90 : 70
+                    for(let i = 1; i <= lvlMax; i++)
+                    {
+                        let tmpStat = weapon.stats(i)
+                        stats.push({
+                            ascension: tmpStat.ascension,
+                            attack: tmpStat.attack,
+                            level: i,
+                            specialized: tmpStat.specialized
+                        })
+                        if([20, 40, 50, 60, 70, 80].includes(i)) {
+                            tmpStat = weapon.stats(i, '+')
+                            stats.push({
+                                ascension: tmpStat.ascension,
+                                attack: tmpStat.attack,
+                                level: i,
+                                specialized: tmpStat.specialized
+                            })
+                        }
+                    }
+                    weapon.stats = stats
+                    let tmp = new WeaponFRModel(weapon)
+                    tmp.save((err, docs) => {
+                        if(err) console.log("Weapon save error : " + err)
+                    })
+                })
+            }
+            else {
+                if((new Date()).getTime() - docs.insertDate.getTime() > 10000/*86400000*/) {
+                    let weapons = genshin.weapons('names', {matchCategories: true, verboseCategories: true, resultLanguage: 'French'})
+                    weapons.forEach(weapon => {
+                        let stats = []
+                        let lvlMax = weapon.rarity > 2 ? 90 : 70
+                        for(let i = 1; i <= lvlMax; i++)
+                        {
+                            let tmpStat = weapon.stats(i)
+                            stats.push({
+                                ascension: tmpStat.ascension,
+                                attack: tmpStat.attack,
+                                level: i,
+                                specialized: tmpStat.specialized
+                            })
+                            if([20, 40, 50, 60, 70, 80].includes(i)) {
+                                tmpStat = weapon.stats(i, '+')
+                                stats.push({
+                                    ascension: tmpStat.ascension,
+                                    attack: tmpStat.attack,
+                                    level: i,
+                                    specialized: tmpStat.specialized
+                                })
+                            }
+                        }
+                        weapon.stats = stats
+                        WeaponFRModel.findOneAndReplace({name: weapon.name}, weapon).exec()
+                    })
+                }
+            }
+        })
+        setTimeout(() => {
+            WeaponFRModel.find((err, docs) => {
+                if(err) console.log('Weapon find error : ' + err)
+                else res.send(docs)
+            })
+        }, 100);
     }
 })
 
