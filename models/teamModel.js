e6a8fa23172b0ad163530978c90c41585ed4bd77
LRojon <loic.rojon@gmail.com>
2021-08-17 17:02:00 +0200

    Add Team Build

---
+++ models/teamModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    characters: [
+        {
+            character: String,
+            role: String
+        }
+    ]
+})
+const TeamENModel = mongoose.model('team', schema, 'teamsEN')
+const TeamFRModel = mongoose.model('team', schema, 'teamsFR')
+
+module.exports = { TeamENModel, TeamFRModel }
